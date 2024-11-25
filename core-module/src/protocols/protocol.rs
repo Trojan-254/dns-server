ff9d053675953a43e372995cf85f8f04a6c67558
Samwuel Simiyu <samwuels254@gmail.com>
2024-11-25 10:57:06 +0300

    Update protocol.rs
    implemented result code

--- core-module/src/protocols/protocol.rs
+++ core-module/src/protocols/protocol.rs
@@ -580,13 +580,54 @@ impl DnsRecord {
             | DnsRecord::TXT { ttl: TransientTtl(ttl), .. } => ttl,
             DnsRecord::OPT { .. } => 0,
         }
+    }    
+}
+
+
+/// The result code for a DNS query, as described in the specification
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum ResultCode {
+    NOERROR = 0,
+    FORMERR = 1,
+    SERVFAIL = 2,
+    NXDOMAIN = 3,
+    NOTIMP = 4,
+    REFUSED = 5,
+    UNKNOWN(u8), // Handle unsupported result codes
+}
+
+impl Default for ResultCode {
+    fn default() -> Self {
+        ResultCode::NOERROR
+    }
+}
+
+impl ResultCode {
+    /// Create a `ResultCode` from a numeric value
+    pub fn from_num(num: u8) -> ResultCode {
+        match num {
+            0 => ResultCode::NOERROR,
+            1 => ResultCode::FORMERR,
+            2 => ResultCode::SERVFAIL,
+            3 => ResultCode::NXDOMAIN,
+            4 => ResultCode::NOTIMP,
+            5 => ResultCode::REFUSED,
+            _ => ResultCode::UNKNOWN(num), // Handle unknown codes gracefully
+        }
     }
 
-
-
-
-
-    
+    /// Get the numeric representation of the `ResultCode`
+    pub fn to_num(&self) -> u8 {
+        match *self {
+            ResultCode::NOERROR => 0,
+            ResultCode::FORMERR => 1,
+            ResultCode::SERVFAIL => 2,
+            ResultCode::NXDOMAIN => 3,
+            ResultCode::NOTIMP => 4,
+            ResultCode::REFUSED => 5,
+            ResultCode::UNKNOWN(num) => num,
+        }
+    }
 }
 
 
