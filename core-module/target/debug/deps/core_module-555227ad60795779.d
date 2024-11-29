9a970bdc58b46d29a7dbcd797d1b8f8b2f46326e
Samwuel simiyu <samwuels254@gmail.com>
2024-11-29 14:43:21 +0300

    header test

--- core-module/target/debug/deps/core_module-555227ad60795779.d
+++
@@ -1,7 +0,0 @@
-/home/sam/dns-server/core-module/target/debug/deps/libcore_module-555227ad60795779.rmeta: src/lib.rs
-
-/home/sam/dns-server/core-module/target/debug/deps/libcore_module-555227ad60795779.rlib: src/lib.rs
-
-/home/sam/dns-server/core-module/target/debug/deps/core_module-555227ad60795779.d: src/lib.rs
-
-src/lib.rs:
--- core-module/target/debug/deps/core_module-c7336c05f675433e.d
+++
@@ -1,5 +0,0 @@
-/home/sam/dns-server/core-module/target/debug/deps/core_module-c7336c05f675433e: src/lib.rs
-
-/home/sam/dns-server/core-module/target/debug/deps/core_module-c7336c05f675433e.d: src/lib.rs
-
-src/lib.rs:
