824d54b2c6f8aff06b23a9ac8d1ca85be8d754de
Samwuel Simiyu <samwuels003@gmail.com>
2024-12-05 13:46:00 +0300

    File modules

--- core-module/src/lib.rs
+++ core-module/src/lib.rs
@@ -1,2 +1,7 @@
 pub mod buffer;
 pub mod protocols;
+pub mod cache;
+pub mod client;
+pub mod monitoring;
+pub mod resolvers;
+pub mod server;
---
+++ core-module/src/resolvers/mod.rs
@@ -0,0 +1,3 @@
+pub mod resolve;
+pub mod forwading_resolver;
+pub mod recursive_resolver;
