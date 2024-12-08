fbe378bf918935c8132cdd2bf73f11df36ec1836
Samwuel Simiyu <samwuels254@gmail.com>
2024-12-08 15:35:46 +0300

    Update netutil.rs

--- core-module/src/network_utilities/netutil.rs
+++ core-module/src/network_utilities/netutil.rs
@@ -3,7 +3,7 @@
 use std::io::{Read, Result, Write};
 use std::net::TcpStream;
 
-pub fn read_packet_length(stream: &mut TcpStream) -> Result<u16> {
+pub async fn read_packet_length(stream: &mut TcpStream) -> Result<u16> {
     let mut len_buffer = [0; 2];
     stream.read(&mut len_buffer)?;
     let length = ((len_buffer[0] as u16) << 8) | (len_buffer[1] as u16);
@@ -11,7 +11,7 @@ pub fn read_packet_length(stream: &mut TcpStream) -> Result<u16> {
     Ok(length)
 }
 
-pub fn write_packet_length(stream: &mut TcpStream, len: usize) -> Result<()> {
+pub async fn write_packet_length(stream: &mut TcpStream, len: usize) -> Result<()> {
     let mut len_buffer = [0; 2];
     len_buffer[0] = (len >> 8) as u8;
     len_buffer[1] = (len & 0xFF) as u8;
